14d936c6331eff83810897d8cdb49046f2892541
Felix Onen <onenfelix90@gmail.com>
2022-09-22 18:31:07 +0300

    Learn strings

--- strings/basics.go
+++ strings/basics.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main () {
-	fmt.Println("We are learning strings")
+	var name = "Felix Onen, is a software engineer";
+	fmt.Println(name);
+	fmt.Println(len(name));
+	fmt.Println(name[:2]);
+	var newstring = name[:];
+	fmt.Println(newstring);
+	fmt.Println(strings.HasPrefix(name, "Fe"));
+	fmt.Println(strings.Count(name, "en"));
+	fmt.Printf("%q\n", strings.Split(name, " "))
 }
